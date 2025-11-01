b7d9aa36bb650ffc3bfc516474441eaee4e92ddb
Alberto-zero <ledezma.valdez.alberto@gmail.com>
2025-10-31 18:58:56 -0600

    conexion a bd en cloude bien

--- db.js
+++ db.js
@@ -1,13 +1,15 @@
-const mysql = require('mysql2');
+require('dotenv').config();
+const mysql = require('mysql2/promise'); // usa la versión con promesas
 
 const pool = mysql.createPool({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  port: process.env.MYSQL_PORT || 3306,
+  connectionLimit: 4,
+  ssl: { rejectUnauthorized: false }
 });
 
-module.exports = pool.promise();
+
+module.exports = pool;
